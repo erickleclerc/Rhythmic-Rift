a9a7d18d4fb03fb780d47cde9f3c05a3a77392fe
RachelChao <drawinsky@gmail.com>
2023-06-07 16:26:34 -0700

    Add music box

--- Assets/99-RachelFolder/02_Scripts/Bullet.cs
+++ Assets/99-RachelFolder/02_Scripts/Bullet.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    
+    public GameObject spark;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Box") 
+        {
+            GameObject.Destroy(collision.gameObject);
+            GameObject.Destroy(gameObject);
+
+            GameObject S = Instantiate(spark, collision.gameObject.transform.position, Quaternion.identity);
+        }
+    }
 
 }
