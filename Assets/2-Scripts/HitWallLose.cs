7eb0cd69dd6e96d54f12e7c8deb9bbfe35811d93
erickleclerc <107086802+erickleclerc@users.noreply.github.com>
2023-08-03 15:31:24 -0700

    Minor Adjustments

--- Assets/2-Scripts/HitWallLose.cs
+++ Assets/2-Scripts/HitWallLose.cs
@@ -6,14 +6,21 @@ using UnityEngine.SceneManagement;
 public class HitWallLose : MonoBehaviour
 {
     [SerializeField] private GameObject gameOverText;
+    [SerializeField] private GameObject[] otherTexts;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Wall")
         {
             Destroy(other.gameObject);
+
+            foreach (GameObject otherText in otherTexts)
+            {
+                otherText.SetActive(false);
+            }
+
             gameOverText.SetActive(true);
-            gameOverText.GetComponent<TextMeshPro>().text = "You hit a wall! Try Again!";
+            gameOverText.GetComponent<TextMeshPro>().text = "Hit a wall! Try Again!";
 
             //PAUSE EVERTYING
             Time.timeScale = 0;
